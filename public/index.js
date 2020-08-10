f0705d2d6391c21db20a4396d25f1be51006de10
xiaofeng <804875728@qq.com>
2020-08-10 14:40:46 +0800

    增加导航栏

---
+++ public/index.js
@@ -0,0 +1,12 @@
+$(".slide__link").on("click", (e) => {
+  window.location.href = "slide.html";
+});
+$(".stickey__link").on("click", (e) => {
+  window.location.href = "stickey.html";
+});
+$(".suggestion__link").on("click", (e) => {
+  window.location.href = "suggestion.html";
+});
+$(".tab__link").on("click", (e) => {
+  window.location.href = "tab.html";
+});
