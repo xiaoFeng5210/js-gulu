50390ff9746b4482d0cdac2b9a9070f34b15de16
xiaofeng <804875728@qq.com>
2020-08-13 14:18:11 +0800

    eventHub基本完成

--- src/eventHub.ts
+++ src/eventHub.ts
@@ -1,21 +1,28 @@
 class EventHub {
   cache = {};
   on(eventName, fn) {
-    if (this.cache[eventName] === undefined) {
-      this.cache[eventName] = [];
-    }
-    const array = this.cache[eventName];
-    array.push(fn);
+    this.cache[eventName] = this.cache[eventName] || [];
+    this.cache[eventName].push(fn);
   }
-  emit(eventName) {
-    let array = this.cache[eventName];
-    if (array === undefined) {
-      array = [];
-    }
-    array.forEach((fn) => {
-      fn();
-    });
+  emit(eventName, data?) {
+    (this.cache[eventName] || []).forEach((fn) => fn(data));
+  }
+  off(eventName, fn) {
+    this.cache[eventName] = this.cache[eventName] || [];
+    let index = indexFind(this.cache[eventName], fn);
+    if (index === -1) return;
   }
 }
 
 export default EventHub;
+
+function indexFind(array, item) {
+  let index = -1;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      index = i;
+      break;
+    }
+  }
+  return index;
+}
