9893e6cc74b554065140dc8ee09459fb91ee6e98
xiaofeng <804875728@qq.com>
2020-08-13 13:28:24 +0800

    eventHub on事件完成

--- src/eventHub.ts
+++ src/eventHub.ts
@@ -1,3 +1,13 @@
-class EventHub {}
+class EventHub {
+  cache = {};
+  on(eventName, fn) {
+    if (this.cache[eventName] === undefined) {
+      this.cache[eventName] = [];
+    }
+    const array = this.cache[eventName];
+    array.push(fn);
+  }
+  emit() {}
+}
 
 export default EventHub;
